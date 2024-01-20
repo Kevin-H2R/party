cc9ac5284a77610901f8160b9083880c47cbd0cc
Kevin <herrbach.kevin@gmail.com>
2024-01-20 16:30:39 +0900

    Displays coming/past parties for the logged user on the dashboard

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,5 +1,39 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 const Dashboard = () => {
-  return <></>
+  const [coming, setComing] = useState([])
+  const [past, setPast] = useState([])
+  useEffect(() => {
+    const fetchDashboard = async () => {
+      const response = await axios.get(import.meta.env.VITE_API_URL + '/dashboard')
+      const coming = []
+      const past = []
+      const today = new Date()
+      response.data.forEach(p => {
+        const date = new Date(p.start_date)
+        date < today ? past.push(p) : coming.push(p)
+      })
+      setComing(coming)
+      setPast(past)
+    }
+    fetchDashboard()
+  }, [])
+  return <div>
+    <div>Coming</div>
+    {coming.map(c => <>
+      <div>{c.name}</div>
+      <div>{c.start_date}</div>
+      <div>{c.capacity}</div>
+    </>)}
+    <div>Past</div>
+    
+    {past.map(p => <>
+      <div>{p.name}</div>
+      <div>{p.start_date}</div>
+      <div>{p.capacity}</div>
+    </>)}
+  </div>
 }
 
 export default Dashboard
