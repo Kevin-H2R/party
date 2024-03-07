54ee9eabcc461666bc242808a58adb7e721006c1
Kevin <herrbach.kevin@gmail.com>
2024-03-07 14:26:09 +0900

    Improves login design a bit

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -21,17 +21,17 @@ const Login = () => {
   return <LoginContainer className="flex h-screen justify-center items-center p-2">
     <div className="card flex flex-col items-center w-full md:w-9/12 lg:w-6/12">
       <div className="flex p-6 w-full title justify-center">Welcome back!</div>
-      <form className="flex flex-col p-6 gap-2 w-full">
+      <form className="flex flex-col p-6 gap-4 w-full">
         <input type="email" className="login-input placeholder:text-sm placeholder:font-medium"
           value={email} placeholder="Email"
           onChange={e => setEmail(e.target.value)}/>
         <input type="password" className="login-input placeholder:text-sm placeholder:font-medium"
           value={password} placeholder="Password"
           onChange={e => setPassword(e.target.value)}/>
-        <Link to="/register" className="register-link text-center">Signup</Link>
         <Button $primary type="submit" onClick={e => {e.preventDefault(); login()}}>
-          Login
+          Log In
         </Button>
+        <Link to="/register" className="text-sm">Don't have an account? Sign up</Link>
       </form>
     </div>
     </LoginContainer>
